de936125874bf0865a576131d50668730d1e7e56
JoanMendo <147327997+JoanMendo@users.noreply.github.com>
2024-02-01 13:50:02 +0100

    Add files via upload

--- Mastermind_ENUNCIADO/script.js
+++ Mastermind_ENUNCIADO/script.js
@@ -1,5 +1,5 @@
 //Declaración de constantes.
-const MAX_INTENTOS = 6;
+const MAX_INTENTOS = 8;
 const MAX_COMBI_COLORES = 4;
 const COLORS = ['white', 'blue', 'green', 'violet', 'yellow', 'red', 'orange', 'cyan'];
 const GREY = "grey";
@@ -9,21 +9,21 @@ const BLACK = "black";
 
 /** Template con el código HTML que corresponde a cada fila de juego/intento. */
 const ROW_RESULT = `
-<div class="rowResult w100 flex wrap">
-    <div class="rowUserCombi w75 flex wrap">
-       <div class="w25">
-           <div class="celUserCombi flex"></div>
+<div class="rowResult w100 flex wrap center">
+    <div class="rowUserCombi w100 flex wrap">
+       <div class="w25 flex">
+           <div class="cel flex"></div>
+       </div>
+       <div class="w25 flex">
+           <div class="cel flex"></div>
        </div>
        <div class="w25">
-           <div class="celUserCombi flex"></div>
+           <div class="cel flex"></div>
        </div>
        <div class="w25">
-           <div class="celUserCombi flex"></div>
+           <div class="cel flex"></div>
        </div>
-       <div class="w25">
-           <div class="celUserCombi flex"></div>
-       </div>
-    </div>alis/Mastermind_CODIGO
+    </div>==========================================================================
     <div class="rowCercleResult w25 flex wrap center">
        <div class="w40 h40">
             <div class="cercleResult flex"></div>
@@ -42,17 +42,22 @@ const ROW_RESULT = `
 
 
 //Declaración de variables globales.
+let imputColors = document.getElementsByClassName("cel flex");
+let cercleResult = document.getElementsByClassName("cercleResult flex");
 const master = [];
 const userCombi = [];
 var intento = 0;
 var aciertos = 0;
 var currentColor = 0;
+let currentTry = 0;
 
 function init() {
     //1. Genera el código random del master
 
     for (let i = 0; i < MAX_COMBI_COLORES; i++) {
-    master [i] = Math.floor(Math.random() * 7);
+    master [i] = COLORS[Math.floor(Math.random() * 7)];
+    imputColors[i].style.border = "1px solid black";
+    
     }
     //2. Crea todas las filas según el número de intentos.
     for (let i = 0; i < MAX_INTENTOS; i++) {
@@ -70,6 +75,49 @@ introducido el usuario.
 Informamos al usuario del resultado y del número de intentos que lleva*/
 function Comprobar() {
     let firstTry = document.getElementsByClassName("w100 info");
+    if (currentColor < MAX_COMBI_COLORES) {
+        firstTry[0].textContent = "Debes introducir 4 colores";
+        firstTry[0].style.display = "block";
+    }
+    else
+    {
+        firstTry[0].style.display = "none";
+        currentColor = 0;
+        for (let i = 12 ; i < 16 ; i++) {
+            imputColors[i + (4 * currentTry)].style.backgroundColor = userCombi[i-(12)];
+            imputColors[i-(12)].style.backgroundColor = "grey";
+            if (userCombi[i-(12)] == "white") {
+                imputColors[i + (4 * currentTry)].style.border = "1px solid black";
+               
+            }
+
+            if (userCombi[i-(12)] == master[i-(12)]) {
+                cercleResult[i-(12) + (4 * currentTry)].style.backgroundColor = "black";
+            }
+            else if (master.includes(userCombi[i-(12)])) {
+                cercleResult[i-(12) + (4 * currentTry)].style.backgroundColor = "white";
+            }
+            else {
+                cercleResult[i-(12) + (4 * currentTry)].style.backgroundColor = "grey";
+            }
+        }
+        currentTry++;
+        if (master.toString() == userCombi.toString()) {
+            alert("Has ganado");
+            window.location.reload();
+        }
+        if (currentTry == MAX_INTENTOS) {
+            alert("Has perdut");
+            for (let i = 0 ; i < 4 ; i++) {
+                imputColors[i].style.backgroundColor = master[i];
+            }
+            firstTry[0].textContent = "Has perdut, aquesta era la combinació correcta";
+            firstTry[0].style.display = "block";
+
+        }
+    }
+    
+
 
     
 }
@@ -77,13 +125,12 @@ function Comprobar() {
 /** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
 function añadeColor(color) {
     if (currentColor < MAX_COMBI_COLORES) {
-        let imputColors = document.getElementsByClassName("cel flex");
+        
         imputColors[currentColor].style.backgroundColor = color;
         userCombi[currentColor] = color;
-        currentColor++;
-    }
-   
+        
+    currentColor++;
 
 }
-
+}
 
